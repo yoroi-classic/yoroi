7e57a08dda90ddedfa46a02499665a9aef79cce0
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-07-15 11:09:22 -0400

    Stabilize standalone common package checks

--- scripts/packages/common/bob.config.js
+++ scripts/packages/common/bob.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   source: 'src',
   output: 'lib',
+  exclude: '**/{__tests__,__fixtures__,__mocks__,*.test.*,*.spec.*}{,/**}',
   targets: [
     'commonjs',
     'module',
--- scripts/packages/common/jest.config.js
+++ scripts/packages/common/jest.config.js
@@ -1,16 +1,22 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  roots: ['<rootDir>/../../../mobile/packages/common'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-native-async-storage/async-storage|@testing-library/react-native|@tanstack/react-query)/)'
+    'node_modules/(?!(react-native|@react-native|@react-native-async-storage/async-storage|@testing-library/react-native|@tanstack/react-query|react-error-boundary)/)',
   ],
   setupFiles: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/node_modules'],
+  moduleNameMapper: {
+    '^@yoroi/logger$': '<rootDir>/../logger/src',
+    '^@yoroi/types$': '<rootDir>/../types/src',
+  },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
   collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts',
-    '!src/fixtures/**'
+    '!src/fixtures/**',
   ],
   coverageReporters: ['text-summary', 'lcov', 'html'],
   coverageThreshold: {
@@ -18,15 +24,15 @@ module.exports = {
       branches: 100,
       functions: 100,
       lines: 100,
-      statements: 100
-    }
+      statements: 100,
+    },
   },
   modulePathIgnorePatterns: [
     '<rootDir>/example/node_modules',
-    '<rootDir>/lib/'
+    '<rootDir>/lib/',
   ],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
-  testEnvironment: 'jsdom'
-} 
+  testEnvironment: 'jsdom',
+}
